698205f0619cf6888b053c749e1686f1a1496651
RapidFresh <rubentapadas1998@gmail.com>
2018-12-17 11:40:12 +0000

    changed typo

--- login.php
+++ login.php
@@ -83,7 +83,7 @@
           <ul class="navbar-nav ml-auto">
 
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <a class="nav-link dropdown-toggle pr-2" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Design Your Product
               </a>
               <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
